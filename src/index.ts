3628ea6d76477b55e469f7ea720a317c8ff5fecb
nurfianqodar <77nurfianqodar@gmail.com>
2024-11-29 18:53:10 +0000

    fix router

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,18 @@
 import express, { type Application } from "express";
 import { APP_PORT } from "./configs";
 import { appsErrorLogger, appsLogger } from "@/utils/logger";
-import usersExtrasApp from "@/apps/users";
+import usersRouter from "@/apps/users";
 // import scansApp from "@/apps/scans";
-import platsApp from "@/apps/plants";
+// import platsApp from "@/apps/plants";
 // import diseasesApp from "@/apps/diseases";
 
 const app: Application = express();
 
 app.use(appsErrorLogger);
 app.use(appsLogger);
-app.use("/", usersExtrasApp);
-app.use("/", platsApp);
+
+app.use("/", usersRouter);
+// app.use("/", platsApp);
 
 app.listen(APP_PORT, () => {
 	console.log(`App running on ${APP_PORT}.`);
